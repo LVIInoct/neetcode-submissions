fe8cd8a8b644a20b46c59f1f1643b1246815ff29
@noctiluzenit <bonbietotallypro@gmail.com>
2026-08-27 09:50:44 -0300

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,44 @@
+# Declare node properties
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {} # map k to node
+        # LRU & MRU
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # LRU removal
+    def remove(self, node):
+        prev, next = node.prev, node.next
+        prev.next, next.prev = next, prev # skipping node for removal
+    # MRU insertion
+    def insert(self, node):
+        prev, next = self.right.prev, self.right
+        prev.next = next.prev = node # updating pointers
+        node.next, node.prev = next, prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val # to not return key, return val
+        return -1
+
+    # update value
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        # remove node from the DLL and delete LRU from cache if capcity exceeds
+        if len(self.cache) > self.cap:
+            lru = self.left.next # LRU
+            self.remove(lru)
+            del self.cache[lru.key]
